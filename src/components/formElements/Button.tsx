f0252454011afb68867a3fe0f838061bd532124f
Dorian Edwards <dorian.m.edwards@gmail.com>
2024-03-18 23:20:35 -0400

    add preliminary button component

---
+++ src/components/formElements/Button.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react'
+
+export default function Button({
+  children,
+  variant,
+}: {
+  children: ReactNode
+  variant: number | string
+}) {
+  return (
+    <button
+      className={
+        'text-[1.3rem] font-bold py-[1.5rem] px-[3.2rem] uppercase w-full max-w-[60rem] tracking-[1px] ' +
+        buttonStyles[variant]
+      }
+    >
+      {children}
+      {variant === 3 || variant === '3' ? <ChevronRight /> : null}
+    </button>
+  )
+}
+
+const buttonStyles: { [key: number | string]: string } = {
+  1: 'text-white bg-caramel hover:bg-atomic_tangerine hover:cursor-pointer',
+  2: 'text-black bg-white border border-black hover:text-white hover:bg-black',
+  3: 'text-gray hover:text-caramel flex items-center justify-center gap-x-[1.3rem]',
+}
+
+export function ChevronRight() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={4}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='m8.25 4.5 7.5 7.5-7.5 7.5'
+      />
+    </svg>
+  )
+}
