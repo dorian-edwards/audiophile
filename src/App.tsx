45512d318c6be3a6ca3c763156aea669c3970925
Dorian Edwards <dorian.m.edwards@gmail.com>
2024-04-12 09:37:12 -0400

    start building mobile ZX7 page preview card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import Canvas from './Canvas'
-import PagePreview from './components/PagePreviewCard'
+import Button from './components/formElements/Button'
+import Text from './components/text/Text'
 
 function App() {
   return (
     <Canvas lightMode={true}>
       <div className='container max-w-[111rem] mx-auto'>
-        <PagePreview />
+        <PagePreview2 />
       </div>
     </Canvas>
   )
 }
 
 export default App
+
+export function PagePreview2() {
+  return (
+    <div className='w-[90%] mx-auto py-[10rem] pl-[2.4rem] bg-no-repeat bg-cover bg-[url(./images/home/mobile/image-speaker-zx7.jpg)]'>
+      <div className='content'>
+        <Text
+          type='h2Mobile'
+          sx={{
+            color: '#000',
+            marginBottom: '3.4rem',
+            letterSpacing: '2px',
+            fontSize: '2.8rem',
+          }}
+        >
+          ZX7 Speaker
+        </Text>
+        <Button
+          variant={2}
+          sx={{ maxWidth: '18rem', backgroundColor: 'transparent' }}
+        >
+          See product
+        </Button>
+      </div>
+    </div>
+  )
+}
