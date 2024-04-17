ce135073202a4216ad99774881b81d8897bb7dfc
Dorian Edwards <dorian.m.edwards@gmail.com>
2024-04-17 14:20:23 -0400

    add classname prop to Button component

--- src/components/formElements/Button.tsx
+++ src/components/formElements/Button.tsx
@@ -3,21 +3,20 @@ import ChevronRight from '../../icons/ChevronRight'
 
 export default function Button({
   children,
+  className,
   variant,
   sx,
   svgSx,
 }: {
   children: ReactNode
+  className?: string | undefined
   variant: number | string
   sx?: React.CSSProperties | undefined
   svgSx?: React.SVGAttributes<SVGSVGElement>
 }) {
   return (
     <button
-      className={
-        'text-[1.3rem] font-bold py-[1.5rem] px-[3.2rem] uppercase w-full max-w-[60rem] tracking-[1px] ' +
-        buttonStyles[variant]
-      }
+      className={`text-[1.3rem] font-bold py-[1.5rem] px-[3.2rem] uppercase w-full max-w-[60rem] tracking-[1px] ${buttonStyles[variant]}  ${className}`}
       style={sx}
     >
       {children}
