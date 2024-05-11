d20fd1d2e3a8e72e120143e610d919976612a0a8
Dorian Edwards <dorian.m.edwards@gmail.com>
2024-05-10 22:53:53 -0400

    adjust responsive stylings of ProductImage component

--- src/components/pages/ProductPage.tsx
+++ src/components/pages/ProductPage.tsx
@@ -23,7 +23,7 @@ export default function ProductPage() {
 
 export function ProductImage() {
   return (
-    <div className='product-image__img-wrapper w-full bg-cover bg-center bg-no-repeat bg-[url(./images/products/product-xx99-mark-two-headphones/mobile/image-product.jpg)] aspect-square mx-auto tablet2:pb-0 shrink-0 tablet2:w-[48rem] tablet2:max-w-[28rem] tablet2:bg-[url(./images/products/product-xx99-mark-two-headphones/tablet/image-product.jpg)] mb-[3.2rem] tablet2:mb-0 tablet2:aspect-auto desktop2:bg-[url(./images/products/product-xx99-mark-two-headphones/desktop/image-product.jpg)] desktop2:w-[54rem] desktop2:max-w-[54rem] desktop2:aspect-[27/28] desktop2:h-[56rem]' />
+    <div className='product-image__img-wrapper w-full bg-cover bg-center bg-no-repeat bg-[url(./images/products/product-xx99-mark-two-headphones/mobile/image-product.jpg)] tablet2:bg-[url(./images/products/product-xx99-mark-two-headphones/tablet/image-product.jpg)] desktop2:bg-[url(./images/products/product-xx99-mark-two-headphones/desktop/image-product.jpg)] aspect-square tablet2:pb-0 shrink-0 tablet2:max-w-[28rem] mb-[3.2rem] tablet2:mb-0 tablet2:aspect-[281/480] desktop2:max-w-[54rem] desktop2:aspect-[27/28] mx-auto' />
   )
 }
 
