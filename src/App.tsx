1572a70ada9a3b941b103af099ec34baf7639af1
Dorian Edwards <dorian.m.edwards@gmail.com>
2024-05-23 05:40:56 -0400

    add scroll snap to page top on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import BaseLayout from './components/layouts/BaseLayout'
+import { useEffect } from 'react'
+
+export function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 function App() {
   return (
     // <Canvas lightMode={true}>
-    <BaseLayout>
-      <Outlet />
-    </BaseLayout>
+    <>
+      <ScrollToTop />
+      <BaseLayout>
+        <Outlet />
+      </BaseLayout>
+    </>
     // </Canvas>
   )
 }
